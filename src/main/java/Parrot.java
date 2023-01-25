a2430241606775ab064172330f97207d5e70e8ba
kaleabeabera <Kabera@colgate.edu>
2023-01-25 03:07:34 +0000

    Completed Method Parameters

--- src/main/java/Parrot.java
+++ src/main/java/Parrot.java
@@ -2,7 +2,7 @@
 public class Parrot {
     /**
      * This method should return whatever String was passed into it by whatever invoked it (in this case, the test),
-     * instead of returning "return text please!"
+     * instead of  returning "return text please!" 
      *
      * for example:
      * String str = copy("hello");
